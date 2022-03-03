defa6a086b94b226d717705b716e394d4e3a4809
DANI MI <zeleugim@gmail.com>
2022-03-03 13:27:15 +0100

    2022/03/03

--- optica.js
+++ optica.js
@@ -8,118 +8,134 @@ db.clientes.drop();
  */
 db.proveedores.insertMany(
     [
-        {
-            _id: 1,
-            nombre: "Rayban",
-            telefono: "611111111",
-            fax: "611111111",
-            nif: "61111111A",
-            direccion: {
-                calle: 'Aaaa',
-                numero: 1,
-                piso: 1,
-                puerta: 1,
-                ciudad: "Amsterdam",
-                codigopostal: "11111",
-                pais: "Holanda"
-            }
-        },
-        {
-            _id: 2,
-            nombre: "Hawckers",
-            telefono: "622222222",
-            fax: "622222222",
-            nif: "62222222A",
-            direccion: {
-                calle: 'Bbbb',
-                numero: 2,
-                piso: 2,
-                puerta: 2,
-                ciudad: "Birmingham",
-                codigopostal: "22222",
-                pais: "Inglaterra"
-            },
-        },
-        {
-            _id: 3,
-            nombre: "AwesomeGlasses",
-            telefono: "633333333",
-            fax: "633333333",
-            nif: "63333333A",
-            direccion: {
-                calle: 'Cccc',
-                numero: 3,
-                piso: 3,
-                puerta: 3,
-                ciudad: "Cordoba",
-                codigopostal: "33333",
-                pais: "España"
-            }
-        }
+     {
+       _id: 1,
+       nombre: "Rayban",
+       telefono: "611111111",
+       fax: "611111111",
+       nif: "61111111A",
+       direccion:{
+         calle: 'Aaaa',
+         numero: 1,
+         piso: 1,
+         puerta: 1,
+         ciudad: "Amsterdam",
+         codigopostal: "11111",
+         pais: "Holanda"
+       }
+     },
+    {
+       _id: 2,
+       nombre: "Hawckers",
+       telefono: "622222222",
+       fax: "622222222",
+       nif: "62222222A",
+       direccion:{
+         calle: 'Bbbb',
+         numero: 2,
+         piso: 2,
+         puerta: 2,
+         ciudad: "Birmingham",
+         codigopostal: "22222",
+         pais: "Inglaterra"
+       },
+     },
+      {
+       _id: 3,
+       nombre: "AwesomeGlasses",
+       telefono: "633333333",
+       fax: "633333333",
+       nif: "63333333A",
+       direccion:{
+         calle: 'Cccc',
+         numero: 3,
+         piso: 3,
+         puerta: 3,
+         ciudad: "Cordoba",
+         codigopostal: "33333",
+         pais: "España"
+       }
+     }
     ]
-)
-
-
-db.clientes.insertMany(
+   )
+   
+   
+   db.clientes.insertMany(
     [
-        {
-            _id: 1,
-            nombre: "Pepito",
-            telefono: "644444444",
-            email: "pepito@outlook.com",
-            fecha_registro: ISODate("2021-01-03T23:30:15.123Z"),
-            recomendado_por: "",
-            fecha_venta: ISODate("2021-01-03T23:30:15.123Z"),
-            empleado_venta: "Manolo",
-            direccion: {
-                calle: 'Dddd',
-                numero: 4,
-                piso: 4,
-                puerta: 4,
-                ciudad: "Donetsk",
-                codigopostal: "44444",
-                pais: "Ucrania"
-            },
-            gafa_comprada: {
-                marca: "Blue Rayban",
-                graduacion_izq: 0.25,
-                graduacion_der: 0.25,
-                tipo_montura: "flotante",
-                color_montura: "azul",
-                color_cristales: "azul",
-                precio: 25,
-                proveedor_id: 1
-            }
-        },
-        {
-            _id: 2,
-            nombre: "Grillo",
-            telefono: "655555555",
-            email: "grillo@outlook.com",
-            fecha_registro: ISODate("2021-02-03T23:30:15.123Z"),
-            recomendado_por: "Pepito",
-            fecha_venta: ISODate("2021-02-03T23:30:15.123Z"),
-            empleado_venta: "Manolo",
-            direccion: {
-                calle: 'Eeee',
-                numero: 5,
-                piso: 5,
-                puerta: 5,
-                ciudad: "Edimburgo",
-                codigopostal: "55555",
-                pais: "Escocia"
-            },
-            gafa_comprada: {
-                marca: "Green Rayban",
-                graduacion_izq: 0.5,
-                graduacion_der: 0.5,
-                tipo_montura: "pasta",
-                color_montura: "verde",
-                color_cristales: "verde",
-                precio: 50,
-                proveedor_id: 1
-            }
-        }
-    ]
-)
-
+     {
+       _id: 1,
+       nombre: "Pepito",
+       telefono: "644444444",
+       email: "pepito@outlook.com",
+       fecha_registro: ISODate("2021-01-03T23:30:15.123Z"),
+       recomendado_por: ObjectId("5349b4ddd2781d08c09890f4"),
+       fecha_venta: ISODate("2021-01-03T23:30:15.123Z"),
+       empleado_venta: "Manolo",
+       direccion:{
+         calle: 'Dddd',
+         numero: 4,
+         piso: 4,
+         puerta: 4,
+         ciudad: "Donetsk",
+         codigopostal: "44444",
+         pais: "Ucrania"
+       },
+         gafas_compradas:
+         [
+           {
+           marca: "Blue Rayban",
+           graduacion_izq: 0.25,
+           graduacion_der: 0.25,
+           tipo_montura: "flotante",
+           color_montura: "azul",
+           color_cristales: "azul",
+           precio: 25,
+           proveedor_id: 1
+           }
+         ]
+     },
+     {
+       _id: 2,
+       nombre: "Grillo",
+       telefono: "655555555",
+       email: "grillo@outlook.com",
+       fecha_registro: ISODate("2021-02-03T23:30:15.123Z"),
+       recomendado_por: ObjectId("5349b4ddd2781d08c09890f4"),
+       fecha_venta: ISODate("2021-02-03T23:30:15.123Z"),
+       empleado_venta: "Manolo",
+       direccion:{
+         calle: 'Eeee',
+         numero: 5,
+         piso: 5,
+         puerta: 5,
+         ciudad: "Edimburgo",
+         codigopostal: "55555",
+         pais: "Escocia"
+       },
+         gafas_compradas:
+         [
+           {
+             marca: "Green Rayban",
+             graduacion_izq: 0.5,
+             graduacion_der: 0.5,
+             tipo_montura: "pasta",
+             color_montura: "verde",
+             color_cristales: "verde",
+             precio: 50,
+             proveedor_id: 1
+           },
+           {        
+             marca: "Green Rayban",
+             graduacion_izq: 0.5,
+             graduacion_der: 0.5,
+             tipo_montura: "pasta",
+             color_montura: "verde",
+             color_cristales: "verde",
+             precio: 50,
+             proveedor_id: 1
+           }
+         ]
+     }
+    ] 
+   )
+   
--- pizzeria.js
+++ pizzeria.js
@@ -8,66 +8,98 @@ db.clientes.drop(); */
 
 db.tiendas.insertMany(
     [
-        {
-            _id: 1,
-            direccion: {
-                id_direccion: 545,
-                calle: 'Aaaa',
-                numero: 1,
-                codigopostal: "11111",
-                localidad: "Sabadell",
-                provincia: "Barcelona"
-            },
-            productos: {
-                id_producto: 343,
-                tipo: "pizza",
-                nombre: "Hawaiana",
-                descripcion: "Piña y queso",
-                imagen: "http://olegif.com/bin/gifs/00/49/30.gif",
-                precio: 12.95,
-                categoria_pizza: "Gourmet"
-            },
-            empleados: {
-                id_empleado: 234,
-                nombre: "Manuela",
-                apellidos: "Zuckerberg",
-                nif: "10444444A",
-                telefono: "634343434",
-                tipo_empleado: "vendedor"
-            }
-        }
+     {
+       _id: 111,
+       direccion:{
+         id_direccion: 545,
+         calle: 'Aaaa',
+         numero: 1,
+         codigopostal: "11111",
+         localidad: "Sabadell",
+         provincia: "Barcelona"
+       },
+   
+       empleados:
+       [
+         {
+         id_empleado: 234,
+         nombre: "Manuela",
+         apellidos: "Zuckerberg",
+         nif: "10444444A",
+         telefono: "634343434",
+         tipo_empleado: "vendedor"
+         },
+         {
+         id_empleado: 235,
+         nombre: "Manuel",
+         apellidos: "Bezos",
+         nif: "10444444B",
+         telefono: "634343435",
+         tipo_empleado: "vendedor"
+         }
+       ],
+       productos_id: 
+       [
+         {id_producto: 343}, {id_producto: 35}
+       ]
+     }
     ]
-)
-
-db.clientes.insertMany(
+   )
+   
+   db.productos.insertMany(
+     [
+       {
+         id_producto: 343,
+         tipo: "pizza",
+         nombre: "Hawaiana",
+         descripcion: "Piña y queso",
+         imagen: "http://olegif.com/bin/gifs/00/49/30.gif",
+         precio: 12.95,
+         categoria_pizza: "Gourmet"
+       },
+           {
+         id_producto: 35,
+         tipo: "hamburguesa",
+         nombre: "3 carnes",
+         descripcion: "Pollo, Conejo, Ternera",
+         imagen: "http://olegif.com/bin/gifs/00/49/30.gif",
+         precio: 14.95,
+         categoria_pizza: ""
+       }
+     ]
+   )
+   
+   db.clientes.insertMany(
     [
-        {
-            _id: 1,
-            nombre: "Pepito",
-            apellidos: "Grillo",
-            telefono: "678787878",
-            fecha_registro: new Date(2021, 01, 01),
-            email: "pepito@outlook.com",
-            direccion: {
-                calle: 'Xxxx',
-                numero: 99,
-                codigopostal: "22222",
-                localidad: "Granollers",
-                provincia: "Barcelona"
-            },
-            pedidos: {
-                pedido_id: 1,
-                fecha: new Date(2021, 01, 01),
-                hora: ("21:26:17Z"),
-                precio: 12.95,
-                entrega: {
-                    empleado_id: 234,
-                    hora_entrega: ("22:00:00"),
-                    fecha_entrega: new Date(2021, 01, 01)
-                }
-            }
-        }
+     {
+       _id: 1,
+       nombre: "Pepito",
+       apellidos: "Grillo",
+       telefono: "678787878",
+       fecha_registro: new Date(2021,01,01),
+       email: "pepito@outlook.com",
+       direccion:{
+         calle: 'Xxxx',
+         numero: 99,
+         codigopostal: "22222",
+         localidad: "Granollers",
+         provincia: "Barcelona"
+       },
+       pedidos: {
+         pedido_id: {_id: 1},
+         tienda_id: {_id: 1},
+         fecha: new Date(2021,01,01),
+         hora: ("21:26:17Z"),
+         precio: 12.95,
+         entrega: {
+           empleado_id: { _id: 234},
+           hora_entrega: ("22:00:00"),
+           fecha_entrega: new Date(2021,01,01)
+         },
+         id_productos_comprados: [
+           {_id: 343},{_id: 343},{_id:35}
+         ]
+       }
+     }
     ]
-)
-
-
+   )
